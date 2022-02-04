9c43eaacc5bdc002e3b84bd5bcb6698ed84c0de7
DhoundiyalShivam <shivamdhoundiyal2@gmail.com>
2022-02-04 16:37:31 +0530

    rockPaperScissors

--- rockPaperScissors/script.js
+++ rockPaperScissors/script.js
@@ -2,12 +2,83 @@ const submit = document.querySelector("#submit")
 const playArea=document.querySelector(".playArea")
 const icons=document.querySelector(".icons")
 const message =document.querySelector("#message")
+const playerScore = document.querySelector("#playerScore")
+const computerScore = document.querySelector("#computerScore")
+const playerSelect = document.querySelector("#playerSelect")
+const computerSelect = document.querySelector("#computerSelect")
 
 submit.addEventListener("click",startGame)
-
+let compArr=['rock','paper','scissors']
+let pscore=0;
+let cscore=0;
+let game=false
 function startGame(){
+    game=true
     submit.style.display="none"
     playArea.style.display="block"
     icons.style.display="block"
-
+    displayMessage("Game Started")
 }
+function displayMessage(msg){
+message.innerText = msg
+}
+function getPlayerSelection(playerSelection){
+    let compSelection=getCompSelection()
+playerSelect.innerHTML=`<i class="fas fa-hand-${playerSelection}"></i>`
+computerSelect.innerHTML=`<i class="fas fa-hand-${compSelection}"></i>`
+ scores(playerSelection,compSelection)
+}
+function resetGame(){
+    console.log(5)
+}
+function scores(pselect,cselect){
+    if(pselect==5 || cscore==5){
+        game=false
+        resetGame()
+    }
+  if(game){
+    if(pselect===cselect){
+        playerScore.innerText=pscore
+        computerScore.innerText=cscore
+        displayMessage('DRAW')
+    }else if(pselect=="rock" && cselect=="scissors"){
+        pscore++
+        playerScore.innerText=pscore
+        displayMessage('Player Won this round')
+    }else if(pselect=="rock" && cselect=="paper"){
+        cscore++
+        computerScore.innerText=cscore
+        displayMessage('Computer Won this round')
+
+    }else if(pselect=="paper" && cselect =="scissors"){
+        cscore++
+        computerScore.innerText=cscore
+        displayMessage('Computer Won this round')
+    }else if(pselect=="paper" && cselect =="rock"){
+        pscore++
+        playerScore.innerText=pscore
+        displayMessage('Player Won this round')
+
+    }else if(pselect=="scissors" && cselect=="rock"){
+        cscore++
+        computerScore.innerText=cscore
+        displayMessage('Computer Won this round')
+    }else if(pselect=="scissors" && cselect=="paper"){
+        pscore++
+        displayMessage('Player Won this round')
+        playerScore.innerText=pscore
+    }
+  }
+}
+
+function getCompSelection(){
+    return compArr[Math.floor(Math.random()*compArr.length)]
+}
+const rock = document.querySelector("#rock")
+const paper = document.querySelector("#paper")
+const scissors = document.querySelector("#scissors")
+
+rock.addEventListener("click",()=>getPlayerSelection(rock.id))
+paper.addEventListener("click",()=>getPlayerSelection(paper.id))
+scissors.addEventListener("click",()=>getPlayerSelection(scissors.id))
+
