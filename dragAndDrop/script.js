119e05968e9d190fc75a7d7e2319e94a0139dd34
DhoundiyalShivam <shivamdhoundiyal2@gmail.com>
2022-02-10 01:01:32 +0530

    dragAndDrop

---
+++ dragAndDrop/script.js
@@ -0,0 +1,35 @@
+const itemBoxes=document.querySelectorAll(".itemsBox")
+const items=document.querySelectorAll(".item")
+
+
+items.forEach((item)=>{
+    item.addEventListener("dragstart",function(){
+        draggedItem=item;
+        setTimeout(()=>{
+        item.style.display="none"
+        },0)
+    })
+    item.addEventListener("dragend",function(){
+
+        item.style.display="block"
+
+        setTimeout(()=>{
+
+            draggedItem=null
+        })
+    })
+    itemBoxes.forEach((itemBox)=>{
+        itemBox.addEventListener("dragover",function(e){
+            e.preventDefault()
+        })
+        itemBox.addEventListener("dragenter",function(e){
+            e.preventDefault()
+        })
+        itemBox.addEventListener("drop",function(){
+            this.append(draggedItem)
+        })
+    })
+})
+
+//we have set the item to display nnone before we are dragging it.so the item will be there but not visible to us 
+// so we have used settimeout
