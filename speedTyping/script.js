91a3c4a1c20803875e4d0c5bdc905bfbaa5b6312
DhoundiyalShivam <shivamdhoundiyal2@gmail.com>
2022-02-09 17:17:08 +0530

    speedTyping

---
+++ speedTyping/script.js
@@ -0,0 +1,72 @@
+const loader=document.querySelector(".loader")
+const container=document.querySelector(".container")
+const text=document.querySelector(".toBeTyped")
+const textArea = document.querySelector(".typeText")
+const timer=document.querySelector(".timer")
+const api="https://type.fit/api/quotes"
+let quoteArr=[]
+function showLoader(){
+    container.style.display="none"
+    loader.style.display="block"
+}
+function removeLoader(){
+    
+    container.style.display="block"
+    loader.style.display="none"
+}
+function showQuote(){
+    text.innerHTML=""
+    quote=quoteArr[Math.floor(Math.random()*quoteArr.length)]
+   let singleText=quote.text.split("")
+singleText.forEach((char)=>{
+    const span=document.createElement("span")
+    span.innerText=char
+text.appendChild(span)
+startTimer()
+})
+}
+function startTimer(){
+     date=new Date()
+
+setInterval(()=>{
+    timer.textContent=`${getTime()}s`
+},1000)
+}
+function getTime(){
+    return Math.floor((new Date()-date)/1000)
+}
+async function getTypingText(){
+    showLoader()
+ try{
+    let response = await fetch(api)
+     quoteArr= await response.json()
+    showQuote()
+}catch(e){
+     console.log(e)
+ }
+ removeLoader()
+   
+}
+function compareText(){
+    let singleCharArr=document.querySelectorAll("span")
+    let textAreaChar=textArea.value.split("")
+    singleCharArr.forEach((char,index)=>{
+        let character = textAreaChar[index]
+       if(character == null){
+        char.classList.remove("correct")
+        char.classList.remove("incorrect")
+       }
+        else if(character==char.innerText){
+            char.classList.add("correct")
+            char.classList.remove("incorrect")
+        }
+        else{
+            char.classList.add("incorrect")
+            char.classList.remove("correct")
+        }
+    })
+}
+textArea.addEventListener("input",compareText)
+getTypingText()
+console.log(new Date() - new Date())
+
