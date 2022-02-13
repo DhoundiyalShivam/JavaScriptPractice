6f3fcd032fa1ddad84ab5775ed572387811160a0
DhoundiyalShivam <shivamdhoundiyal2@gmail.com>
2022-02-14 01:28:56 +0530

    done

--- weatherApp/script.js
+++ weatherApp/script.js
@@ -21,7 +21,7 @@ let desc=document.querySelector('.desc')
 
 
 let city
-
+let celcius=true
 
 async function weatherDetails(api){
    try{
@@ -30,20 +30,29 @@ async function weatherDetails(api){
     displayDetails(data)
 }catch(error){
        console.log(error.message)
+       alert('Please Enter a valid city name')
    }
 }
 function displayDetails(data){
-console.log(data)
 timeZone.textContent=data.location.tz_id
 icon.src=data.current.condition.icon
-temprature.textContent=data.current.temp_c + 'c'
+celcius?temprature.textContent=data.current.temp_c + 'c':temprature.textContent=data.current.temp_f + 'f';
 desc.textContent=data.location.region
-// desc.textContent='delhi'
 }
+// temprature.addEventListener('click',()=>{
+//     celcius=!celcius
+//     console.log(celcius)
+    
+// })
 submit.addEventListener('click',()=>{
-    city=input.value
-    console.log(city)
+    if(input.value){
+        city=input.value
+    }
+    else{
+        alert('Please Enter the city name')
+    }
 let api = `http://api.weatherapi.com/v1/current.json?key=4425302281a941dea5a181626221302&q=${city}&aqi=no`
 
     weatherDetails(api)
 })
+weatherDetails(`http://api.weatherapi.com/v1/current.json?key=4425302281a941dea5a181626221302&q=delhi&aqi=no`)
