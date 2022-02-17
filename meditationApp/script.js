23a016cf02a5ee0b7905a94cc654732433d1111c
DhoundiyalShivam <shivamdhoundiyal2@gmail.com>
2022-02-17 17:54:31 +0530

    circleAnimationRemaining

--- meditationApp/script.js
+++ meditationApp/script.js
@@ -6,20 +6,24 @@ const rain=document.querySelector(".rain")
 const beach=document.querySelector(".beach")
 const time=document.querySelectorAll(".time")
 const timer=document.querySelector(".timer")
+let defaultDuration=10
 time.forEach((singleTime)=>{
 singleTime.addEventListener('click',()=>{
-    let fakeTimeDuration=singleTime.dataset.time
-    let sec=fakeTimeDuration%60;
-    let min=fakeTimeDuration/60;
-
+     defaultDuration=singleTime.dataset.time
+     let sec=Math.floor(currentTime%60).toString().padStart(2,0);
+     let min=Math.floor(currentTime/60).toString().padStart(2,0);
     timer.textContent=`${min}:${sec}`
 })
 })
 function beachSound(){
+    play.style.display="block"   
+    pause.style.display="none"
     audio.src='./audio/beach.mp3'
 video.src='./video/beach1.mp4'
 }
 function rainSound(){
+    play.style.display="block"   
+    pause.style.display="none"
 audio.src='./audio/rain.mp3'
 video.src='./video/rain1.mp4'
 }
@@ -34,6 +38,21 @@ function playSound(){
     pause.style.display="block"
     video.play()
     audio.play()
+}
+audio.ontimeupdate=function(){
+    let elapsedTime=audio.currentTime
+    let currentTime=defaultDuration-elapsedTime
+    let sec=Math.floor(currentTime%60).toString().padStart(2,0);
+    let min=Math.floor(currentTime/60).toString().padStart(2,0);
+    timer.textContent=`${min}:${sec}`
+    if(elapsedTime>=defaultDuration){
+    timer.textContent=`00:00`
+    video.pause()
+    audio.pause()
+    audio.currentTime=0
+    play.style.display="block"   
+    pause.style.display="none"
+    }
 }
 play.addEventListener('click',playSound)
 pause.addEventListener('click',pauseSound)
