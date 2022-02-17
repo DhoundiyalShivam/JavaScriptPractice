720d26fcfe8fa5199cd6b8457b049bb547bae407
DhoundiyalShivam <shivamdhoundiyal2@gmail.com>
2022-02-17 13:34:14 +0530

    complete

--- BookmarkerApp/script.js
+++ BookmarkerApp/script.js
@@ -3,14 +3,13 @@ const urlName = document.querySelector("#urlName")
 const submit = document.querySelector("#submit")
 const show = document.querySelector(".outputContainer")
 if(JSON.parse(localStorage.getItem('bookmarks'))){
-    console.log('true')
     showBookmark()
 }
 function getValues() {
     let website = websiteName.value
     let url = urlName.value
-    // if(website && url){
-    // console.log(website,url)
+    if(website && url){
+    console.log(website,url)
     let bookmark = {
         siteName: website,
         siteUrl: url
@@ -24,35 +23,33 @@ let bookmarks = []
     } else {
   let bookmarks = JSON.parse(localStorage.getItem('bookmarks'))
         bookmarks.push(bookmark)
-        localStorage.setItem('bookmarks', JSON.stringify(bookmarks))
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks))
     showBookmark()
-
-
     }
-
-    // }else{
-    //     alert("Please Enter the value")
-    // }
+    }else{
+        alert("Please Enter the value")
+    }
 }
 function deleteBookmark(url){
-    console.log(url)
     let bookmarks = JSON.parse(localStorage.getItem('bookmarks'))
     bookmarks.forEach((el,i)=>{
         if(url==el.siteUrl){
+            console.log(i)
             bookmarks.splice(i,1)
         }
     })
+    localStorage.setItem('bookmarks', JSON.stringify(bookmarks))
+    showBookmark() 
+
 }
 function showBookmark() {
    let bookmarks = JSON.parse(localStorage.getItem('bookmarks'))
-   let bookmark = document.createElement('div')
-    bookmarks.forEach(el=>{
-        let siteName=el.siteName
-        let siteUrl=el.siteUrl
-        bookmark.classList.add('bookmark')
-    bookmark.innerHTML=`<h3>${siteName}</h3>  <a class="btn btn-primary mx-3" href="${siteUrl}" target="_blank">Visit</a>  <button onclick="deleteBookmark('${siteUrl}')" class="btn btn-danger">Delete</button>`
-    })
-    show.appendChild(bookmark)
+   show.innerHTML=''
+   bookmarks.forEach(el=>{
+    let siteName=el.siteName
+    let siteUrl=el.siteUrl
+    show.innerHTML +=`<div class="bookmark"><h3>${siteName}</h3>  <a class="btn btn-primary" href="${siteUrl}" target="_blank">Visit</a>  <button onclick="deleteBookmark('${siteUrl}')" class="btn btn-danger">Delete</button></div>`
+})
 
 
 }
